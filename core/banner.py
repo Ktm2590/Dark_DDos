bbe5d37482dc54e0fa532d14e64433ef57405c7a
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-10-06 23:10:41 +0300

    Update and rename core/banner.py to  core/banner.py

--- core/banner.py
+++ core/banner.py
@@ -1,1 +1,1 @@
-banner = '+---------------+ \n|   Dark_DDos   |  \n|  Version 1.0  |  \n|   DarkGamer   | \n+---------------+'
+banner = '+---------------+ \n|   Dark_DDos   |  \n|  Version 1.1  |  \n|   DarkGamer   | \n+---------------+'
