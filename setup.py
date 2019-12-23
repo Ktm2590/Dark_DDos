c2ce4c54ad5b37c57d0d625e26ababfa0500952b
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-12-23 13:27:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os, sys
 try:
 	arg=sys.argv[1]
 	if arg.lower()=="install":
-		os.system("echo install package; apt install wget git python -y &> log.txt; echo install modules; pip3 install -r requirements.txt &> log.txt")
+		os.system("echo install package; apt install figlet wget git python -y &> log.txt; echo install modules; pip3 install -r requirements.txt &> log.txt")
 	else:
 		print('''
 Не верный аргумент!
